75cf900b2c40d89e682abc65febce806a0866149
tport@caltech.edu <tport@caltech.edu>
2021-10-06 06:15:30 +0000

    almost

--- src/recover.c
+++ src/recover.c
@@ -5,8 +5,27 @@
 #include "fat16.h"
 
 const size_t MASTER_BOOT_RECORD_SIZE = 0x20B;
+const size_t FILE_ALLOCATION_TABLES_SIZE = 78 * 512;
+
+void recurse_follow(directory_entry_t *entry, directory_node_t *node,
+                    bios_parameter_block_t *bpb, size_t entry_index) {
+    if (!is_hidden(entry[entry_index])) {
+        if (is_directory(entry[entry_index])) {
+            node->
+
+                init_directory_node();
+        }
+        else {
+        }
+    }
+    else {
+        return;
+    }
+}
 
 void follow(FILE *disk, directory_node_t *node, bios_parameter_block_t bpb) {
+    directory_entry_t *cast_entry = (directory_entry_t *) disk;
+    recurse_follow(cast_entry, node, &bpb, 0);
     (void) disk;
     (void) node;
     (void) bpb;
@@ -27,6 +46,9 @@ int main(int argc, char *argv[]) {
     bios_parameter_block_t bpb;
 
     /* TODO: Write your code here. */
+    fseek(disk, MASTER_BOOT_RECORD_SIZE, SEEK_SET);
+    fread(&bpb, 1, sizeof(bios_parameter_block_t), disk);
+    fseek(disk, get_root_directory_location(bpb), SEEK_SET);
 
     directory_node_t *root = init_directory_node(NULL);
     follow(disk, root, bpb);
